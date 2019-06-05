27ce136a8ae5465782b0ee1e0101550b94d51fcb
Marco Bruno <marco.bruno.br@gmail.com>
2019-06-05 22:01:11 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edite o <code>src/App.js</code> e save para relaod.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React
         </a>
       </header>
     </div>
