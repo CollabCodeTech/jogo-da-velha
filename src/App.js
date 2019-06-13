fb81847bcd335c0460afa8bed03622477473a194
Marco Bruno <marco.bruno.br@gmail.com>
2019-06-13 19:30:29 +0100

    Fizemos o link sobre

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite o <code>src/App.js</code> e save para relaod.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React
-        </a>
-      </header>
-    </div>
+    <a href="#to-do" className="action">Sobre</a>
   );
 }
 
