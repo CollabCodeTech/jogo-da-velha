968ef504cd90042e4f37ce1a40967f9ccf4903e8
Marco Bruno <marco.bruno.br@gmail.com>
2019-06-14 21:26:37 +0100

    Comecamos a nossa arquitetura de pastas, usando a ITCSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/settings/colors.css';
+import './styles/generic/reset.css';
+import './styles/elements/base.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
