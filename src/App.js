c23e06f3a69d8e808995b67b6b953fe8431ce1d9
Marco Bruno <marco.bruno.br@gmail.com>
2019-06-26 23:02:25 +0100

    Criou o Logo e adicionamos ele no App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Sobre from './objects/Sobre';
+import Logo from './objects/Logo';
 
 function App() {
   return (
-    <Sobre />
+    <>
+      <Logo />
+      <Sobre />
+    </>
   );
 }
 
---
+++ src/objects/Logo/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import CollabCode from '../../img/logo.png';
+
+const Logo = () => (<img src={CollabCode} alt="Logo da CollabCode" />);
+
+export default Logo;
