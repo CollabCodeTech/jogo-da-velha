a249e94e47a67eeb9640354f35113aa52287c8ba
Marco Bruno <marco.bruno.br@gmail.com>
2019-06-27 22:34:46 +0100

    Criado o menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Sobre from './objects/Sobre';
 import Logo from './objects/Logo';
+import Sobre from './objects/Sobre';
+import Menu from './objects/Menu';
 
 function App() {
   return (
     <>
       <Logo />
       <Sobre />
+      <Menu />
     </>
   );
 }
---
+++ src/objects/Menu/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+const Menu = () => (
+    <a href="#menu" className="menu"><span className="center">Menu</span></a>
+);
+
+export default Menu;
