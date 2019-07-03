fbf6c8d4061a0ecd00161789ffe3ab465350b47a
Marco Bruno <marco.bruno.br@gmail.com>
2019-07-03 22:31:30 +0100

    Criamos o nosso object Player

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Card from './objects/Card';
+import Player from './objects/Player';
 import Header from './components/Header';
+
 
 function App() {
   return (
     <>
       <Header />
       <Card />
+      <Player />
     </>
   );
 }
---
+++ src/objects/Player/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Player = () => (
+    <button className="player">X</button>
+);
+
+export default Player;
