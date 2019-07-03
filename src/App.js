189fc4419e9d97bf9948840403b7e3c27fbeda50
Marco Bruno <marco.bruno.br@gmail.com>
2019-07-03 22:56:35 +0100

    Aceitar dois jogares no Player

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <>
       <Header />
       <Card />
-      <Player />
+      <Player player="o" />
     </>
   );
 }
--- src/objects/Player/index.js
+++ src/objects/Player/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import './styles.css';
+import playerX from '../../img/player-x.png';
+import playerO from '../../img/player-o.png';
 
-const Player = () => (
-    <button className="player">X</button>
-);
+const Player = ({player}) => {
+    const players = [];
+    players['x'] = playerX;
+    players['o'] = playerO;
+
+    return (<button className="player">
+        <img src={players[player]} alt={`Jogador ${player.toUpperCase()}`} />
+    </button>)
+};
 
 export default Player;
