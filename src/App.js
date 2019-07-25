4ea26b222e80ddad880b48d5ced9f123f4c0f0ed
Marco Bruno <marco.bruno.br@gmail.com>
2019-07-25 11:45:19 +0100

    Posicionamos o header e o card no lugar certo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import './App.css';
 import Header from './components/Header';
 import Hashtag from './components/Hashtag';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Hashtag />
-    </>
-  );
-}
+const App = () => (
+  <main className="app">
+    <Header />
+    <Hashtag />
+  </main>
+);
 
 export default App;
