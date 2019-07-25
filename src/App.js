b53f1ae4033f98143ab2b94788bd90e45154f64a
Marco Bruno <marco.bruno.br@gmail.com>
2019-07-25 12:11:07 +0100

    Criou o object Input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 
+import Input from './objects/Input';
 import Label from './objects/Label';
 import Header from './components/Header';
 import Hashtag from './components/Hashtag';
@@ -10,6 +11,7 @@ const App = () => (
   <main className="app">
     <Header />
     <Hashtag />
+    <Input value="show" />
     <Label content="Mostrar eventos" />
   </main>
 );
---
+++ src/objects/Input/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Input = ({ value }) => (
+    <input type="checkbox" value={value} />
+);
+
+export default Input
