5b38fb05a0119ca851b6307b20fdf98d8744d5f1
Marco Bruno <marco.bruno.br@gmail.com>
2019-07-25 17:26:15 +0100

    Criar o checkbox com o before

--- src/objects/Checkbox/index.js
+++ src/objects/Checkbox/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles.css';
 
 import Input from '../Input';
 import Label from '../Label';
--- src/objects/Input/index.js
+++ src/objects/Input/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Input = ({ id = "", value = "", type = "text" }) => (
-    <input id={id} type={type} value={value} />
+    <input className="input" id={id} type={type} value={value} />
 );
 
 export default Input
