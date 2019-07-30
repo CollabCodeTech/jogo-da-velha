ab60d7f8e985d3168824c38dea63225f00fe9f3a
Marco Bruno <marco.bruno.br@gmail.com>
2019-07-30 22:56:40 +0100

    Refatory trocamos o nome da class de sobre para about-link

--- src/objects/AboutLink/index.js
+++ src/objects/AboutLink/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css'
 
 const Sobre = () => {
-   return (<a href="#to-do" className="sobre">Sobre</a>)
+   return (<a href="#to-do" className="about-link">Sobre</a>)
 }
 
 export default Sobre
