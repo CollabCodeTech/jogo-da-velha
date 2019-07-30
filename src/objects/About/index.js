227416719374e368e987c3d61942046a8dc87585
Marco Bruno <marco.bruno.br@gmail.com>
2019-07-30 22:38:00 +0100

    Implementar o CSS do About, mas esta faltando colocar ele na posicao certa

--- src/objects/About/index.js
+++ src/objects/About/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import './styles.css';
 
-const About = () => <article className="about">Marco Bruno</article>;
+const About = () => <article className="about"></article>;
 
 export default About;
