fa2a2d17b1554fd303895e0f7aaf50532c316e2c
Marco Bruno <marco.bruno.br@gmail.com>
2019-07-30 22:32:47 +0100

    Criamos o object About

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import './App.css';
 
 import Checkbox from './objects/Checkbox';
+import About from './objects/About';
 import Header from './components/Header';
 import Hashtag from './components/Hashtag';
-import Input from './objects/Input';
-
 
 const App = () => (
   <main className="app">
     <Header />
     <Hashtag />
     <Checkbox id="show" value="show" type="checkbox" content="Mostrar eventos" />
+
+    <About />
   </main>
 );
 
---
+++ src/objects/About/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const About = () => <article className="about">Marco Bruno</article>;
+
+export default About;
