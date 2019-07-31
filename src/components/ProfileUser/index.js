e8fccad5d495054dbe661e3704697b086c95d3b6
Marco Bruno <marco.bruno.br@gmail.com>
2019-07-31 12:43:11 +0100

    Criamos o object IconSocial

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './styles.css'
 import Profile from '../../img/profile.jpg';
+import GitHub from '../../img/github.png'
+import Linkedin from '../../img/linkedin.png'
+import Facebook from '../../img/facebook.png'
+import Twitter from '../../img/twitter.png'
+import Instagram from '../../img/instagram.png'
 
 import AvatarProfile from '../../objects/AvatarProfile';
+import IconSocial from '../../objects/IconSocial';
 
 const ProfileUser = () => (
     <dl className="profile-user">
@@ -11,6 +17,13 @@ const ProfileUser = () => (
         </dd>
         <dt className="title">Marco Bruno</dt>
         <dd className="description">Migrei de palhaço para Dev. Front-End/UX e agora eu trabalho como streamer de código, instrutor e dev na CollabCode</dd>
+        <dd className="socials">
+            <IconSocial src={GitHub} alt="Logo da GitHub" />
+            <IconSocial src={Linkedin} alt="Logo da Linkedin" />
+            <IconSocial src={Facebook} alt="Logo da Facebook" />
+            <IconSocial src={Twitter} alt="Logo da Twitter" />
+            <IconSocial src={Instagram} alt="Logo da Instagram" />
+        </dd>
     </dl>
 );
 
