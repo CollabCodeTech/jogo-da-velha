a449c6f690ca653c81309a1fe253f7be152b8e53
Marco Bruno <marco.bruno.br@gmail.com>
2019-08-01 15:43:38 +0100

    Preparar o About para receber a variacao onde ele for usado

--- src/objects/About/index.js
+++ src/objects/About/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './styles.css';
 
-const About = ({ children }) => (
-    <article className="about">
+const About = ({ children, className = "" }) => (
+    <article className={`about ${className}`}>
         {children}
     </article>
 );
