01b806d0c0a2a19bce124113c60318e493c9b0c8
Marco Bruno <marco.bruno.br@gmail.com>
2019-08-05 13:25:11 +0100

    Adicionar o evento de click no PlayerGame

--- src/objects/PlayerGame/index.js
+++ src/objects/PlayerGame/index.js
@@ -3,13 +3,17 @@ import './styles.css';
 import playerX from '../../img/player-x.png';
 import playerO from '../../img/player-o.png';
 
+const handleClick = () => {
+    console.log("Vamo que vamo!!!")
+}
+
 const PlayerGame = ({ player = false }) => {
     const players = [];
     players['x'] = playerX;
     players['o'] = playerO;
 
     return (
-        <button className="player-game">
+        <button onClick={handleClick} className="player-game">
             {player && <img src={players[player]} alt={`Jogador ${player.toUpperCase()}`} />}
         </button>
     )
