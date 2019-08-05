a7dfba0bcbcaae7ac73b26717188ca2835a493bc
Marco Bruno <marco.bruno.br@gmail.com>
2019-08-05 13:40:36 +0100

    Preencher o PlayerGame com o player uma fez que ele foi clicado

--- src/objects/PlayerGame/index.js
+++ src/objects/PlayerGame/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import playerX from '../../img/player-x.png';
 import playerO from '../../img/player-o.png';
 
-const handleClick = () => {
-    console.log("Vamo que vamo!!!")
-}
 
 const PlayerGame = ({ player = false }) => {
+    const [statePlayer, setStatePlayer] = useState(player);
     const players = [];
     players['x'] = playerX;
     players['o'] = playerO;
 
+    const handleClick = () => setStatePlayer("x");
+
     return (
         <button onClick={handleClick} className="player-game">
-            {player && <img src={players[player]} alt={`Jogador ${player.toUpperCase()}`} />}
+            {statePlayer && <img src={players[statePlayer]} alt={`Jogador ${statePlayer.toUpperCase()}`} />}
         </button>
     )
 };
