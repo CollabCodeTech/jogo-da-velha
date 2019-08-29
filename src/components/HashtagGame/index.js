981d942d5d3d3430fd3c06750552e2fd944b843f
Marco Bruno <marco.bruno.br@gmail.com>
2019-08-29 16:15:32 +0100

    Cria o tabulero zero com controle do state dos plyers

--- src/components/HashtagGame/index.js
+++ src/components/HashtagGame/index.js
@@ -5,6 +5,17 @@ import PlayerGame from '../../objects/PlayerGame';
 
 const HashtagGame = () => {
     const [nextPlayer, setNextPlayer] = useState("x");
+    const [players, setPlayers] = useState([
+        {id: 1, content: ''},
+        {id: 2, content: ''},
+        {id: 3, content: ''},
+        {id: 4, content: ''},
+        {id: 5, content: ''},
+        {id: 6, content: ''},
+        {id: 7, content: ''},
+        {id: 8, content: ''},
+        {id: 9, content: ''},
+    ]);
 
     const handleClick = () => {
         console.log("Próximo Jogador", nextPlayer);
@@ -14,18 +25,9 @@ const HashtagGame = () => {
 
     return (
         <CardGame>
+            
             <ul className="hashtag-game" onClick={handleClick}>
-                <li className="item"><PlayerGame /></li>
-                <li className="item"><PlayerGame /></li>
-                <li className="item"><PlayerGame /></li>
-
-                <li className="item"><PlayerGame /></li>
-                <li className="item"><PlayerGame /></li>
-                <li className="item"><PlayerGame /></li>
-
-                <li className="item"><PlayerGame /></li>
-                <li className="item"><PlayerGame /></li>
-                <li className="item"><PlayerGame /></li>
+                {players.map(player => <li className="item"><PlayerGame /></li>)}
             </ul>
         </CardGame>
     )
