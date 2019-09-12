18cebfa1c61a44fb2715f00a5fd4e61f0665b319
Marco Bruno <marco.bruno.br@gmail.com>
2019-09-12 18:21:51 +0100

    Adicina o evento de click para o HistoryGame

--- src/components/HistoryGame/index.js
+++ src/components/HistoryGame/index.js
@@ -3,10 +3,12 @@ import "./styles.css";
 
 import TagGame from "../../objects/TagGame";
 
+const changeHistory = () => console.log("Clicou!")
+
 const HistoryGame = ({history}) => (
     <ol className="history-game">
         {history.map((content, key) =>(
-            <li key={key} className="action">
+            <li key={key} className="action" onClick={changeHistory}>
                 <TagGame content={content} />
             </li>
         ))}
